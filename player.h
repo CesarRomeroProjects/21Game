0eaf1a3f771bcfbd713fc35198966f5527777596
Cesar Romero <cromero5503@gmail.com>
2024-01-02 15:33:42 -0800

    Started Player Class

--- player.h
+++ player.h
@@ -2,5 +2,15 @@
 #include <string>
 
 class Player {
-    public:        
+    private:
+        std::string playerName;
+        int playerAge;
+    public:
+        Player(std::string name, int age) : playerName{name}, playerAge{age} 
+        {}
+
+        void introducePlayer() {
+            std::cout << "Hello " << playerName << " to 21!" << std::endl;
+        }
+
 };

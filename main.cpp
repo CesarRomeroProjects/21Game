6e65011869afee3c4ca4cb2543248f5c42ec8f85
Cesar Romero <cromero5503@gmail.com>
2024-01-02 16:52:18 -0800

    Finished setting up rules for player to read.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "player.h"
 
 int main() {
+    std::string name;
+    int age;
+
+    // User is asked to enter their name and age.
+    std::cout << "Please enter your first name: ";
+    std::cin >> name;
+    std::cout << "Please enter your age: ";
+    std::cin >> age;
+
+    // Player class is called.
+    Player player(name, age);
+    player.introducePlayer();
+    player.showRules();
     return 0;
 }
--- player.h
+++ player.h
@@ -10,7 +10,50 @@ class Player {
         {}
 
         void introducePlayer() {
-            std::cout << "Hello " << playerName << " to 21!" << std::endl;
+            std::cout << "Hello " << playerName << " to the game of 21!" << std::endl;
+        }
+
+        void showRules() {
+
+            std::cout << " _______________________________________________________________ " << std::endl;
+
+            std::cout << "|\t\t\t How To Play" << "\t\t\t\t|" << std::endl;
+
+            std::cout << "|_______________________________________________________________|" << std::endl;
+            
+            std::cout << "|\t" << "1) The dealer and each player start with two" << "\t\t|" << std::endl
+                        << "|\t" << "   cards. The dealer's first card faces up, the" << "\t\t|" << std::endl
+                        << "|\t" << "   second faces down. Face cards each count as" << "\t\t|" << std::endl
+                        << "|\t" << "   10, Aces count as 1 or 11, all others count at" << "\t|" << std::endl
+                        << "|\t" << "   face value. An Ace with any 10, Jack, Queen, or" << "\t|" <<std::endl
+                        << "|\t" << "   King is a \"Blackjack.\"" << "\t\t\t\t|" << std::endl;
+            
+            std::cout << "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" << std::endl;
+
+            std::cout << "|\t" << "2) If you have a Blackjack, the dealer pays you" << "\t\t|" << std::endl
+                        << "|\t" << "   one-and-a-half times your bet --- unless the" << "\t\t|" << std::endl
+                        << "|\t" << "   dealer also has a Blackjack, in which case it's a" << "\t|" << std::endl
+                        << "|\t" << "   \"push\" and neither wins." << "\t\t\t\t|" << std::endl;
+
+            std::cout << "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" << std::endl;
+            
+            std::cout << "|\t" << "3) If you don't have Blackjack, you can ask the" << "\t\t|" << std::endl
+                        << "|\t" << "   dealer to \"hit\" you by pressing 0 (No) or 1 (Yes)." << "\t|" << std::endl;
+
+            std::cout << "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" << std::endl;
+
+            std::cout << "|\t" << "4) You can only draw five cards (one at a time)," << "\t|" << std::endl
+                        << "|\t" << "   but if you go over 21, you \"bust\" and lose." << "\t\t|" << std::endl
+                        << "|\t" << "   If you do not want to \"hit\", you may \"stand\"" << "\t\t|" << std::endl
+                        << "|\t" << "   by just pressing 0 (No)." << "\t\t\t\t|" << std::endl;
+
+            std::cout << "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|" << std::endl;
+
+            std::cout << "|\t" << "5) After all players are satisfied with their hands" << "\t|" << std::endl
+                        << "|\t" << "   the dealer will turn his or her down card face" << "\t|" << std::endl
+                        << "|\t" << "   up and stand or draw as necessary." << "\t\t\t|" << std::endl;
+            
+            std::cout << "|_______________________________________________________________|" << std::endl;
         }
 
 };
